cafd83a986dc789b73b000d98d73e630b17ad194
narayanbanke <60840004+narayanbanke@users.noreply.github.com>
2021-01-23 16:59:52 +0530

    Add project files.

---
+++ DapperInMVC/ApllicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using DapperInMVC.Models;
+namespace DapperInMVC
+{
+    public class ApllicationDbContext:DbContext
+    {
+        public ApllicationDbContext() { }   
+        public ApllicationDbContext(DbContextOptions<ApllicationDbContext> options) : base(options) { }
+        public DbSet<DapperInMVC.Models.Employee> Employee { get; set; }
+    
+    }
+}
---
+++ DapperInMVC/Controllers/HomeController.cs
@@ -0,0 +1,62 @@
+﻿using Dapper;
+using DapperInMVC.Models;
+using DapperInMVC.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DapperInMVC.Controllers
+{
+    public class HomeController : Controller
+    {   
+        private readonly IDapper _dapper;
+        public HomeController(IDapper dapper)
+        {
+            _dapper = dapper;
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        public  IActionResult Index()
+        {
+           var Employee =  Task.FromResult(_dapper.GetAll<Employee>($"Select * from [StudentDTO] ", null, commandType: CommandType.Text).ToList());
+            return View(Employee.Result);
+        }
+
+        [HttpPost]
+        [ActionName("Create")]
+        public IActionResult Create_Post(Employee data)
+        {
+            if (ModelState.IsValid)
+            {
+                var dbparams = new DynamicParameters();
+                dbparams.Add("Name", data.Name, DbType.String);
+                dbparams.Add("Age", data.Agr, DbType.Int32);
+                dbparams.Add("City", data.City, DbType.String);
+                var result = Task.FromResult(_dapper.Insert<Employee>("[dbo].[AddNewStudentDTO]"
+                    , dbparams,
+                    commandType: CommandType.StoredProcedure));
+                return RedirectToAction("Index");
+            }
+            else
+                return View();
+        }
+
+        [HttpGet]
+        [ActionName("Details")]
+        public IActionResult Details(int id)
+        {
+            var result =  Task.FromResult(_dapper.Get<Employee>($"Select * from [StudentDTO] where Id = {id}", null, commandType: CommandType.Text));
+            return View(result.Result);
+           
+        }
+
+        }
+}
